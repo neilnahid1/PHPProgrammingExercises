060bcd54132bb3c3dae49524508c8746e9226452
Neil Francis Nahid <neil.nahid@fullspeedtechnologies.com>
2019-07-25 18:14:51 +0800

    fixed palindrome where it doesn't match lower and upper cases

--- ProgrammingExercises/palindrome.php
+++ ProgrammingExercises/palindrome.php
@@ -3,7 +3,7 @@ $input = readline(("Enter input:"));
 function isPalindrome($string){
 $strLength = strlen($string)-1;
     for ($i=0; $i < $strLength; $i++) {
-        if($string[$i]==$string[$strLength-$i]){
+        if(strtolower($string[$i])==strtolower($string[$strLength-$i])){
             if($i==$strLength-1 || ($strLength-$i)-$i==0)
             return true;
             continue;
